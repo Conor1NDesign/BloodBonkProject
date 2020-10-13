18050c126233432feeb32770d08719e4a50d3056
Samuel Le <samuel.le4324@gmail.com>
2020-10-13 15:50:35 +1100

    Added Weapon Detection

---
+++ Assets/_Assets/Scripts/Player/WeaponDetect.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponDetect : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Weapon"))
+        {
+            Debug.Log("Test");
+        }
+    }
+}
