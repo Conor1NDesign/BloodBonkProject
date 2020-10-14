91667b25d0eb732e2006c3ffe1f862d984aa48f3
Samuel Le <samuel.le4324@gmail.com>
2020-10-14 15:10:11 +1100

    Player can now attack

---
+++ Assets/_Assets/Scripts/Player/Weapon.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        DisableWeapon();
+    }
+
+    void Attack()
+    {
+        EnableWeapon();
+        animator.Play("Attack");
+    }
+
+    private void EnableWeapon()
+    {
+        transform.GetComponent<Renderer>().enabled = true;
+        transform.GetComponent<BoxCollider>().enabled = true;
+    }
+
+    public void DisableWeapon()
+    {
+        transform.GetComponent<Renderer>().enabled = false;
+        transform.GetComponent<BoxCollider>().enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Attack();
+        }
+    }
+}
