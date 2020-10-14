8cfbe2fab717c8776fff98b3560551e657aa140a
Samuel Le <samuel.le4324@gmail.com>
2020-10-15 10:04:59 +1100

    Added Powerups

---
+++ Assets/_Assets/Scripts/Player/PowerUp.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum AvaliablePowerUp
+{
+    MaxHealth,
+    MaxDash,
+    AttackSpeed,
+    Lifesteal,
+    Damage
+}
+
+public class PowerUp : MonoBehaviour
+{
+    public AvaliablePowerUp currentPowerUp;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Pickup();
+        }
+    }
+
+    void Pickup()
+    {
+
+        Destroy(gameObject);
+    }
+}
