8f8739da6d564ac57c325c045ef04b2ef684d918
Power <powerfinnthegreat@gmail.com>
2020-10-14 17:44:53 +1100

    Basic enemy spawning

---
+++ Assets/_Assets/Scripts/EnemySpawnPoint.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class EnemySpawnPoint : MonoBehaviour
+{
+    [SerializeField]GameObject enemyPrefab;
+
+    void FixedUpdate()
+    {
+        // Temp debugging spawn key
+        if (Input.GetKeyDown(KeyCode.Backslash))
+        {
+            SpawnEnemy();
+        }
+    }
+
+    void SpawnEnemy()
+    {
+        Instantiate(enemyPrefab, transform.position + Vector3.up, Quaternion.identity);
+    }
+}
