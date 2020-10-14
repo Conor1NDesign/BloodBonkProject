7318e3b82fbbd9cd4bb2b5904075f6937962f505
Samuel Le <samuel.le4324@gmail.com>
2020-10-14 12:32:54 +1100

    Weapon Detection now with color

--- Assets/_Assets/Scripts/Player/WeaponDetect.cs
+++ Assets/_Assets/Scripts/Player/WeaponDetect.cs
@@ -4,11 +4,34 @@ using UnityEngine;
 
 public class WeaponDetect : MonoBehaviour
 {
+    public Material redMat;
+    public Material defaultMat;
+
+    public float colourChangeDelay = 0.5f;
+    float currentDelay = 0f;
+
+    bool colourChangeCollision = false;
+
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Weapon"))
         {
+            colourChangeCollision = true;
             Debug.Log("Test");
+            currentDelay = Time.time + colourChangeDelay;
+        }
+    }
+
+    void Update()
+    {
+        if (colourChangeCollision)
+        {
+            transform.GetComponent<MeshRenderer>().material = redMat;
+            if (Time.time > currentDelay)
+            {
+                transform.GetComponent<MeshRenderer>().material = defaultMat;
+                colourChangeCollision = false;
+            }
         }
     }
 }
