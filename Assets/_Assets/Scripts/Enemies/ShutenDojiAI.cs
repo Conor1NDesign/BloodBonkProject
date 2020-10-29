d047d15509c556782973b1c550c15acfad177a48
Power <powerfinnthegreat@gmail.com>
2020-10-30 10:06:51 +1100

    Better attacking (still temp)

--- Assets/_Assets/Scripts/Enemies/ShutenDojiAI.cs
+++ Assets/_Assets/Scripts/Enemies/ShutenDojiAI.cs
@@ -2,19 +2,25 @@ using UnityEngine;
 
 public class ShutenDojiAI : EnemyAI
 {
+	[SerializeField]float attackLength = 1.0f;
 #pragma warning disable 0649
 	// temp
 	[SerializeField]GameObject weapon;
 #pragma warning restore 0649
+	
 	void FixedUpdate()
 	{
-		if (timeToNextAttack > 0.0f)
+		timeToNextAttack -= 1.0f / 60.0f;
+		if (timeToNextAttack > timeBetweenAttacks - 0.4f)
 		{
-			timeToNextAttack -= 1.0f / 60.0f;
-			weapon.transform.Rotate(new Vector3(0.0f, -2.0f, 0.0f));
+			weapon.transform.Rotate(new Vector3(0.0f, 1f / attackLength, 0.0f));
 		}
-		else
-			weapon.transform.Rotate(new Vector3(0.0f, 120.0f, 0.0f));
+		if (timeToNextAttack > timeBetweenAttacks - attackLength && timeToNextAttack < timeBetweenAttacks - 0.4f)
+		{
+			weapon.transform.Rotate(new Vector3(0.0f, -4.0f / attackLength, 0.0f));
+		}
+		if (timeToNextAttack < timeBetweenAttacks - attackLength)
+			weapon.transform.rotation = new Quaternion(0.0f, 0.0f, 0.0f, 0.0f);
 
 		base.MovementUpdate();
 	}
@@ -26,7 +32,6 @@ public class ShutenDojiAI : EnemyAI
 			// Do the attacky thing
 			//Debug.Log("Attack!");
 			timeToNextAttack = timeBetweenAttacks;
-			weapon.transform.Rotate(-150.0f, 0.0f, 0.0f);
 		}
 	}
 }
