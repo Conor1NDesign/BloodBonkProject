d71fbc15e066f8ec59f407bda944c8adbaded30f
Samuel Le <samuel.le4324@gmail.com>
2020-10-29 16:18:17 +1100

    Powerup collision fix

--- Assets/_Assets/Scripts/Camera/CameraShake.cs
+++ Assets/_Assets/Scripts/Camera/CameraShake.cs
@@ -4,11 +4,8 @@ using UnityEngine;
 
 public class CameraShake : MonoBehaviour
 {
-    float delay;
     public float duration;
     public float magnitude;
-
-    float currentDelay;
 
     public IEnumerator Shake()
     {
@@ -26,26 +23,6 @@ public class CameraShake : MonoBehaviour
             elasped += Time.deltaTime;
 
             yield return null;
-        }
-
-        transform.localPosition = originalPos;
-    }
-
-    public void Shaked()
-    {
-
-        currentDelay = Time.time + delay;
-
-        Vector3 originalPos = transform.localPosition;
-
-        while (Time.time < currentDelay)
-        {
-            float x = Random.Range(-1, 1) * magnitude;
-            float y = Random.Range(-1, 1) * magnitude;
-
-            transform.localPosition = new Vector3(x, y, originalPos.z);
-
-            Debug.Log("while");
         }
 
         transform.localPosition = originalPos;
