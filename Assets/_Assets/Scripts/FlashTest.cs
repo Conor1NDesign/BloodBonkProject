99f41bfc5dd15b26b53f210533b15c6e2f207130
Brandon Russell <brandonru5150@gmail.com>
2020-11-13 14:57:33 +1100

    Auto stash before merge of "master" and "origin/master"

---
+++ Assets/_Assets/Scripts/FlashTest.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashTest : MonoBehaviour
+{
+    public GameObject enemy;
+    Renderer m_ObjectRenderer;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_ObjectRenderer = enemy.GetComponent<Renderer>();
+        m_ObjectRenderer.material.SetColor("_EmissionColor", Color.black);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("g"))
+        {
+            Debug.Log("flash");
+            m_ObjectRenderer.material.SetColor("_EmissionColor", Color.red * 311);
+        }
+        else
+        {
+            Debug.Log("not flash");
+            m_ObjectRenderer.material.SetColor("_EmissionColor", Color.black);
+
+        }
+    }
+}
