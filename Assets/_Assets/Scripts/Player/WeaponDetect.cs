ac0dc097de9dffbdcc61c219df4ee5d4cd3ede17
Samuel Le <samuel.le4324@gmail.com>
2020-11-18 16:51:38 +1100

    Enemy is invincible for a sec after being hit

--- Assets/_Assets/Scripts/Player/WeaponDetect.cs
+++ Assets/_Assets/Scripts/Player/WeaponDetect.cs
@@ -4,18 +4,41 @@ using UnityEngine;
 
 public class WeaponDetect : MonoBehaviour
 {
-
     // Classes
     EnemyAI enemy;
 
+    float currentDelay;
+    bool canBeAttacked;
 
     void Start()
     {
         enemy = GetComponent<EnemyAI>();
     }
 
+    void Update()
+    {
+        if (currentDelay < Time.time)
+        {
+            canBeAttacked = true;
+        }
+        else
+        {
+            canBeAttacked = false;
+        }
+    }
+        
+
     public void TakeDamage(float damage)
     {
-        enemy.TakeDamage(damage);
+        if (currentDelay < Time.time)
+        {
+            currentDelay = Time.time + 0.5f;
+
+            enemy.TakeDamage(damage);
+            
+
+            Debug.Log("hit");
+        }
     }
+        
 }
